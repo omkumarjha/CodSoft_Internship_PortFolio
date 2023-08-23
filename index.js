45de74b2e0338b023102546d6705025148f4e36c
om <omkumar1892002@gmail.com>
2023-08-23 11:53:06 +0530

    Added links section at left

---
+++ index.js
@@ -0,0 +1,9 @@
+const formSubmit = () => {
+    let nameInput = document.querySelector(".nameInput")
+    let emailInput = document.querySelector(".emailInput")
+    let messageInput = document.querySelector(".messageInput")
+
+    nameInput.value = "";
+    emailInput.value = "";
+    messageInput.value = "";
+}
