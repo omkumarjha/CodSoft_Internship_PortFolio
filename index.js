644ae287c2bc081ea7237a4fa0d61b16a916706c
om <omkumar1892002@gmail.com>
2023-10-13 15:02:31 +0530

    Added transition in the navbar

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ const formSubmit = () => {
 }
 
 // Hamburger JS
+// let hamburger = document.querySelector(".svg-parent");
+// let right = document.querySelector(".right");
+// let left = document.querySelector(".left");
+
+// hamburger.addEventListener("click",()=>{
+//     right.classList.toggle("display-none")
+//     left.classList.toggle("display-none")
+// })
+
+
+// For hamburger
+
 let hamburger = document.querySelector(".svg-parent");
-let right = document.querySelector(".right");
 let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+let navbar = document.getElementById("navbar");
 
 hamburger.addEventListener("click",()=>{
-    right.classList.toggle("display-none")
-    left.classList.toggle("display-none")
+    left.classList.toggle("v-hidden")
+    right.classList.toggle("v-hidden")
+    navbar.classList.toggle("h-navbar")
 })
